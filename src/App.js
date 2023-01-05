d78ee3e425f4b145bbe0a629230701dbe93d3cb0
Andra <andra.danalachi@thinslices.com>
2023-01-05 15:03:18 +0200

    Setup basic router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import "./App.css";
 import { EveryLatestExample } from "./pages/EveryLatestExample";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/examples",
+    element: <EveryLatestExample />,
+  },
+  { path: "faq", element: <div>FAQ</div> },
+]);
 
 function App() {
   return (
     <div className="app">
-      <EveryLatestExample />
+      <RouterProvider router={router} />
     </div>
   );
 }
