98cd53cc5ca426926dc1fed4a4eab6cf07882e7e
Andra <andra.danalachi@thinslices.com>
2023-01-17 18:28:18 +0200

    Implement FAQ item deletion

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -1,50 +1,96 @@
 import * as React from "react";
-import { useSelector } from "react-redux";
-import { Button } from "../components/Button";
+import { v4 as uuid } from "uuid";
+import { useDispatch, useSelector } from "react-redux";
 
+import { Button } from "../components/Button";
 import { NavigationBar } from "../components/NavigationBar";
-import { selectFaq, FAQ } from "../redux/faq/slice";
+import {
+  selectFaq,
+  FAQ,
+  addFaq,
+  deleteFaq,
+  updateFaq,
+} from "../redux/faq/slice";
 
 interface ItemProps {
   question: string;
   answer: string;
+  id: string;
+  handleUpdate: () => void;
+  handleDelete: () => void;
 }
 
-const Item = ({ question, answer }: ItemProps) => (
-  <div
-    style={{
-      textAlign: "left",
-      border: "1px solid lightgrey",
-      padding: "0 16px",
-    }}
-  >
-    <h5>{question}</h5>
-    <h6>{answer}</h6>
-  </div>
-);
+const Item = ({
+  question,
+  answer,
+  id,
+  handleUpdate,
+  handleDelete,
+}: ItemProps) => {
+  return (
+    <div className="faq-item">
+      <div>
+        <h5>{question}</h5>
+        <h6>{answer}</h6>
+        <h6>ID: {id}</h6>
+      </div>
+      <div style={{ display: "flex", gap: 16 }}>
+        <Button label="📝" onClick={handleUpdate} />
+        <Button label="❌" onClick={handleDelete} />
+      </div>
+    </div>
+  );
+};
 
 export const FAQs = () => {
   const { faqs } = useSelector(selectFaq);
+  const dispatch = useDispatch();
+
+  const [question, setQuestion] = React.useState("");
+  const [answer, setAnswer] = React.useState("");
+
+  const handleAdd = () => {
+    dispatch(addFaq({ question, answer, id: uuid() }));
+    setQuestion("");
+    setAnswer("");
+  };
+
+  const handleUpdate = (payload: FAQ) => {};
+  const handleDelete = (id: string) => dispatch(deleteFaq(id));
+
   return (
     <div>
       <NavigationBar />
-      <div
-        style={{
-          display: "flex",
-          gap: 16,
-          justifyContent: "center",
-          padding: 16,
-        }}
-      >
-        <input type="textarea" placeholder="What is your question?" />
-        <input type="textarea" placeholder="What is your answer?" />
-        <Button label="Add" onClick={() => {}} />
+      <div className="faq-content">
+        <input
+          type="textarea"
+          placeholder="What is your question?"
+          value={question}
+          onChange={(e) => setQuestion(e.currentTarget.value)}
+        />
+        <input
+          type="textarea"
+          placeholder="What is your answer?"
+          value={answer}
+          onChange={(e) => setAnswer(e.currentTarget.value)}
+        />
+        <Button label="Add" onClick={handleAdd} />
       </div>
-
-      <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
-        {faqs.map((faq: FAQ) => (
-          <Item question={faq.question} answer={faq.answer} />
-        ))}
+      <div className="faq-list">
+        {faqs.map((faq: FAQ) => {
+          return (
+            <Item
+              handleUpdate={() =>
+                handleUpdate({ id: faq.id, question, answer })
+              }
+              handleDelete={() => handleDelete(faq.id)}
+              key={faq.id}
+              id={faq.id}
+              question={faq.question}
+              answer={faq.answer}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/redux/faq/slice.ts
+++ src/redux/faq/slice.ts
@@ -1,22 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface FAQ {
+  id: string;
   question: string;
   answer: string;
 }
 
 const faqs = [
   {
+    id: "faq_1",
     question: "What is Redux Saga?",
     answer:
       "Redux Saga is a JS library built on top of Redux, which handles asynchronous operations like data fetching",
   },
   {
+    id: "faq_2",
     question: "What are Redux Saga effects?",
     answer:
       "Effects or effect creators are functions you can execute as part of a Redux Saga control flow, which return plain JavaScript objects and trigger side effects. They do not perform any action, but instead instruct the middleware how to execute a given action.",
   },
   {
+    id: "faq_3",
     question: "What are declarative effects?",
     answer:
       "Declarative effects are objects yielded within the saga by calling effect creators. These objects contain the description of an action that is to be executed by the middleware.",
@@ -31,11 +35,30 @@ export const faqSlice = createSlice({
   name: "faq",
   initialState,
   reducers: {
-    addFaq: (state, action) => {
-      state.faqs = state.faqs.concat(action.payload);
+    addFaq: (state, action) => ({
+      ...state,
+      faqs: state.faqs.concat(action.payload),
+    }),
+    updateFaq: (state, action) => {
+      state.faqs.reduce((acc: FAQ[], value: FAQ) => {
+        if (value.id !== action.payload.id) {
+          acc.push(value);
+        } else {
+          acc.push({
+            id: action.payload.id,
+            question: action.payload.question ?? value.question,
+            answer: action.payload.answer ?? value.answer,
+          });
+        }
+        return acc;
+      }, []);
     },
-    updateFaq: () => {},
-    deleteFaq: () => {},
+    deleteFaq: (state, action) => ({
+      ...state,
+      faqs: state.faqs.filter((faq) => {
+        return faq.id !== action.payload;
+      }),
+    }),
   },
 });
 
