ae7aebcf49ebd91af8ed400e0004f8c1501ead04
Andra <andra.danalachi@thinslices.com>
2023-01-18 12:55:10 +0200

    Add observations section to ForkExample

--- src/pages/ForkExample.tsx
+++ src/pages/ForkExample.tsx
@@ -7,8 +7,9 @@ import { selectFork, runFork, runFailingFork } from "../redux/fork/slice";
 
 export const ForkExample = () => {
   const dispatch = useDispatch();
-
   const { loading, error, success } = useSelector(selectFork);
+
+  const [showObservations, setShowObservations] = React.useState(false);
 
   return (
     <>
@@ -16,7 +17,21 @@ export const ForkExample = () => {
       <div
         style={{ display: "flex", flexWrap: "wrap", gap: 16, marginTop: 16 }}
       >
-        <h1>Fork example</h1>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+            padding: "16px 0",
+          }}
+        >
+          <h1>Fork example</h1>
+          <Button
+            label={`${showObservations ? "Hide" : "Show"} observations`}
+            onClick={() => setShowObservations(!showObservations)}
+          />
+        </div>
+        {showObservations && <ObservationsSection />}
         <div style={{ display: "flex", width: "100%", gap: 16 }}>
           <Button
             onClick={() => dispatch(runFork())}
@@ -34,3 +49,28 @@ export const ForkExample = () => {
     </>
   );
 };
+
+const ObservationsSection = () => (
+  <div
+    style={{
+      borderRadius: 8,
+      border: "1px solid lightgrey",
+      width: "100%",
+      padding: 16,
+    }}
+  >
+    <p>
+      In the first example, the saga successfully finishes its execution. It
+      waits for each of its 3 forked tasks to finish, before completing its own
+      execution.
+    </p>
+    <p>
+      In the second example, the first forked task is trying to make an invalid
+      API call. The second task makes a valid API call and is the first task
+      that resolves (approx. 2.03s on a slow network). The first task resolves
+      shortly after (approx. 2.06s on a slow network), but throws an error. As
+      such, the third forked task never gets to execute, because the parent saga
+      terminates once it receives the error from its first forked task.
+    </p>
+  </div>
+);
