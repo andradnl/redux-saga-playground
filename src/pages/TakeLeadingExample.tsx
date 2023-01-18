c49084e41039c4752355505479548780ac8b32dd
Andra <andra.danalachi@thinslices.com>
2023-01-18 12:45:14 +0200

    Add observations section to TakeLeadingExample

--- src/pages/TakeLeadingExample.tsx
+++ src/pages/TakeLeadingExample.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavigationBar } from "../components/NavigationBar";
 
+import { Button } from "../components/Button";
+import { NavigationBar } from "../components/NavigationBar";
 import { getPost, selectPosts } from "../redux/posts/slice";
 
 interface CardProps {
@@ -10,9 +11,79 @@ interface CardProps {
   body?: string;
 }
 
+export const TakeLeadingExample = () => {
+  const { selectedPost } = useSelector(selectPosts);
+
+  const getBody = (id: number) =>
+    selectedPost.id === id
+      ? selectedPost.body.slice(0, 90).concat("...")
+      : `Post ${id}`;
+
+  const [showObservations, setShowObservations] = React.useState(false);
+
+  return (
+    <>
+      <NavigationBar />
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 16,
+          marginTop: 16,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+            padding: "16px 0",
+          }}
+        >
+          <h1>takeLeading</h1>
+          <Button
+            label={`${showObservations ? "Hide" : "Show"} observations`}
+            onClick={() => setShowObservations(!showObservations)}
+          />
+        </div>
+        {showObservations && <ObservationsSection />}
+        {[1, 2, 3, 4].map((id: number) => {
+          const isSelected = selectedPost.id === id;
+          return (
+            <Card
+              key={id}
+              bgColor={isSelected ? "forestgreen" : "darkgrey"}
+              id={`${id}`}
+              body={getBody(id)}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const ObservationsSection = () => (
+  <div
+    style={{
+      borderRadius: 8,
+      border: "1px solid lightgrey",
+      width: "100%",
+      padding: 16,
+    }}
+  >
+    <p>
+      We can dispatch multiple actions of the same type (or different) in rapid
+      succession, but the saga that fetches data execute only in response to the
+      first action. As such, only one API request is made, and only after the
+      saga has finished its execution, will it handle any other subsequent
+      actions.
+    </p>
+  </div>
+);
+
 const Card = ({ bgColor, id, body }: CardProps) => {
   const dispatch = useDispatch();
-
   return (
     <div
       onClick={() => dispatch(getPost(id))}
@@ -38,36 +109,5 @@ const Card = ({ bgColor, id, body }: CardProps) => {
         {body}
       </span>
     </div>
-  );
-};
-
-export const TakeLeadingExample = () => {
-  const { selectedPost } = useSelector(selectPosts);
-
-  const getBody = (id: number) =>
-    selectedPost.id === id
-      ? selectedPost.body.slice(0, 90).concat("...")
-      : `Post ${id}`;
-
-  return (
-    <>
-      <NavigationBar />
-      <div
-        style={{ display: "flex", flexWrap: "wrap", gap: 16, marginTop: 16 }}
-      >
-        <h1>takeLeading</h1>
-        {[1, 2, 3, 4].map((id: number) => {
-          const isSelected = selectedPost.id === id;
-          return (
-            <Card
-              key={id}
-              bgColor={isSelected ? "forestgreen" : "darkgrey"}
-              id={`${id}`}
-              body={getBody(id)}
-            />
-          );
-        })}
-      </div>
-    </>
   );
 };
